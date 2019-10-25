5ae6304d58acad759e699d9919272d54e67e4e39
Sharmaine Requerme <srqrme@gmail.com>
2019-10-25 19:55:29 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,7 +2,7 @@ class PigLatinizer
   
   
   def initialize(user_phrase)
-    if @user_phrase.split(" ").length == 1
+    if user_phrase.split(" ").length == 1
       piglatinize_phrase(user_phrase)
     else
       piglatinize_phrase(user_phrase)
