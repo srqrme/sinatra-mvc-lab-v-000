35969a030d066242cc04c06d31e0eb08a65ab508
Sharmaine Requerme <srqrme@gmail.com>
2019-10-25 19:58:42 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -10,6 +10,16 @@ class PigLatinizer
   end
   
   def piglatinize_phrase(user_phrase)
-    
+    if vowel?(user_phrase[0])
+      user_phrase = user_phrase + "w"
+    elsif !vowel?(user_phrase[0]) && !vowel?(user_phrase[1]) && !vowel?(user_phrase[2])
+      user_phrase = user_phrase.slice(3..-1) + user_phrase.slice(0,3)
+    elsif !vowel?(user_phrase[0]) && !vowel?(user_phrase[1])
+      user_phrase = user_phrase.slice(2..-1) + user_phrase.slice(0,2)
+    else
+      user_phrase = user_phrase.slice(1..-1) + user_phrase.slice(0)
+    end
+    user_phrase << "ay"
+  
   end
 end
