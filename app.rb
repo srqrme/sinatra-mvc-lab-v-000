385225e680185578fdbbdbca145b5e3f5645af9c
Sharmaine Requerme <srqrme@gmail.com>
2019-10-25 20:03:17 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,4 +6,11 @@ class App < Sinatra::Base
   get '/' do
     erb :user_input
   end
+  
+  post '/piglatinize' do
+    @piglatiner = PigLatinizer.new.piglatinize(params[:user_phrase])
+
+    erb :piglatinized_result
+  end
+
 end
